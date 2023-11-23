daef656632f98e2ea2b03c9ebdab417eb5785d88
Rémi KORZENIOWSKI <r.korzeniowski@icloud.com>
2023-11-23 11:56:39 +0100

    ci: config.php

---
+++ config.php
@@ -0,0 +1,14 @@
+<?php
+try {
+    $hote = "localhost";
+    $port = "3307";
+    $utilisateur = "root";
+    $motDePasse = "";
+    $nomDeLaBase = "ded";
+    $connexion = new PDO("mysql:host=$hote;port=$port;dbname=$nomDeLaBase", $utilisateur, $motDePasse);
+    $connexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    } catch (PDOException $e) {
+        echo "Erreur de connexion à la base de données: " . $e->getMessage();
+        die();
+    }
+?>
